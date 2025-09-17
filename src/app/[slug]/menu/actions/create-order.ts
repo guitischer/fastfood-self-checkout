5e361cfb596275d1b0a3cec60524c24d0abbdcb3
Guilherme Tischer <104859251+guilherme-tischer-isi@users.noreply.github.com>
2025-09-17 14:18:02 -0300

    feat: add current cart and minor fixes

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { OrderType } from "@prisma/client";
-import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -43,7 +43,7 @@ export const createOrder = async (input: CreateOrderInput) => {
     price: productsWithPrices.find((p) => p.id === product.id)!.price,
   }));
 
-  const order = await db.order.create({
+  await db.order.create({
     data: {
       status: "PENDING",
       customerName: input.customerName,
@@ -62,6 +62,7 @@ export const createOrder = async (input: CreateOrderInput) => {
     },
   });
 
-  revalidatePath(`/${input.slug}/orders`);
-  return order;
+  redirect(
+    `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
+  );
 };
